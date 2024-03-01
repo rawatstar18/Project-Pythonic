2e99d8fe0d2455f6fbdb1005c3d2c9bd0ddfa23f
rawatstar18 <pankajsinghrawathuman@gmail.com>
2024-03-01 15:53:52 +0530

    new learning

---
+++ test1.py
@@ -0,0 +1,23 @@
+#Arithmetic Operators and Data Types
+
+#Adding two numbers
+
+num1 = input("first number : ")
+num2 = input("second number: ")
+
+#When we use input(), it takes whatever we type and stores it as a string.
+#So, the numbers stored in num1 and num2 are treated as text and not as mathematical numbers.
+print ("sum of ", num1 ,'+', num2, 'is' , num1 + num2) 
+
+
+#arithmetic operations
+
+x = 5
+y = 9
+
+print (x + y) #addition
+
+print (x-y) #subrataction
+
+print (x/y) #division
+
