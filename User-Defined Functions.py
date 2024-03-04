3c6673e7c5ff858f4a508d29e136807d3f210554
rawatstar18 <pankajsinghrawathuman@gmail.com>
2024-03-04 15:19:31 +0530

    fucntions attraction

---
+++ User-Defined Functions.py
@@ -0,0 +1,24 @@
+#User-Defined Functions
+
+#structure of function
+#def function_name(parameter1, parameter2): 
+    # Function body
+    # ...
+#    return result
+
+# We use the def keyword to define the function, followed by the function name and a set of parentheses. 
+# The parentheses () may contain parameters, and a colon : at the end of the line that indicates the start of the function body. 
+# The return keyword is used to specify the value that the function should return.
+
+#lets create a fuction for our app
+def add_number(num1,num2):
+    # body of the function
+    sum = num1 + num2
+    return  sum
+
+print (add_number(1,5))
+
+#fuction is working good lets work with our previous file and make fuctions.
+
+
+
