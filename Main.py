c1d8914bbe3821cd2a18475202f367960034a3ac
rawatstar18 <pankajsinghrawathuman@gmail.com>
2024-03-05 14:53:30 +0530

    pending

---
+++ Main.py
@@ -0,0 +1,10 @@
+user_name = input('What is you name : ')
+
+welcome_message = 'Hello! ' + user_name + ' welcome to math quiz.' + ' Please provide answers of the following questions.'
+
+print (welcome_message)
+
+#question 1
+num1 = randint(1,10)
+num2 = randint(1,10)
+op
