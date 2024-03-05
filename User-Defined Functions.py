d75b521de2b79eaa35c634ff151f6b27912ae56c
rawatstar18 <pankajsinghrawathuman@gmail.com>
2024-03-05 11:03:30 +0530

    caluation break hearts

--- User-Defined Functions.py
+++ User-Defined Functions.py
@@ -19,6 +19,36 @@ def add_number(num1,num2):
 print (add_number(1,5))
 
 #fuction is working good lets work with our previous file and make fuctions.
+def print_question(digit,op,digit2):
+    print() # We add this here because we want to add blank line so that we can read question comfortably
+    #body of fuction
+    question = 'what is ' + str(digit) + op + str(digit2) + ' ?'
+    print (question)
 
+def answer(user_answer,correct_answer):
+    #body of fuction
+    print('Your answer is' , user_answer)
+    print('Correct answer is' , correct_answer)
+    
+n1 = 40
+n2 = 5
+print_question ( n1 , '+' , n2)
+user_answer = input ('Ans :')
+correct_ans = n1 + n2
+answer (user_answer , correct_ans)
 
+print_question (n1 , ' - ' ,n2)
+user_answer = input ( 'Ans : ')
+correct_ans = n1 - n2
+answer (user_answer , correct_ans)
+
+print_question (n1 , ' * ' ,n2)
+user_answer = input ( 'Ans : ')
+correct_ans = n1 * n2
+answer (user_answer , correct_ans)
+
+print_question (n1 , ' / ' ,n2)
+user_answer = input ( 'Ans : ')
+correct_ans = int(n1 / n2)
+answer (user_answer , correct_ans)
 
